4cd24ed3eba7a4f37c78b181e7fbea526d6a4266
laba7 <oleglaba97@gmail.com>
2017-02-10 19:32:48 +0200

    Update RegistrationServlet.java

--- src/main/java/com/home/servlet/view/RegistrationServlet.java
+++ src/main/java/com/home/servlet/view/RegistrationServlet.java
@@ -42,47 +42,4 @@ public class RegistrationServlet extends HttpServlet {
         req.setAttribute("model", model);
         req.getRequestDispatcher("layout/jsp/main/registration.jsp").forward(req, resp);
     }
-
-    //   @Override
-//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        String carBrand = req.getParameter("carBrand");
-//        carService.getModel(carBrand);
-/*
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-       HibernateUtil.getSession();
-        String section = req.getParameter("section");
-
-        List<CarBrand> carBrand = (List<CarBrand>)session.createSQLQuery("select CAR_BRAND from CAR_BRAND")
-                .addScalar("CAR_BRAND")
-                .setResultTransformer(Transformers.aliasToBean(CarBrand.class)).list();
-
-
-                */
-    }
-    /*
-      private UserService userService = UserServiceImpl.INSTANCE;
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("layout/jsp/main/registration.jsp").forward(req, resp);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String firstName = req.getParameter("firstName");
-        String lastName = req.getParameter("lastName");
-        String email = req.getParameter("email");
-        String password = req.getParameter("password");
-
-        User user = new User(null, firstName, lastName, email, password);
-        if (userService.registerUser(user)) {
-            req.setAttribute("error_message", String.format("User with such email %s is already exist", user.getEmail()));
-            req.getRequestDispatcher("layout/jsp/main/error.jsp").forward(req, resp);
-        } else {
-            req.getRequestDispatcher("layout/jsp/main/welcome.jsp").forward(req, resp);
-        }
-    }
-     */
-
-//}
+}
